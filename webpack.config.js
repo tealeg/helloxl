063757e88832dfc3a3643f54276fc5f6aeaee489
Geoffrey J. Teale <geoffrey.teale@canonical.com>
2017-04-19 12:54:20 +0200

    Use public folder for generated js

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 var webpack = require('webpack');
 var path = require('path');
 
-var BUILD_DIR = path.resolve(__dirname, 'views/js');
+var BUILD_DIR = path.resolve(__dirname, 'public/js');
 var APP_DIR = path.resolve(__dirname, 'jsx');
 
 var config = {
@@ -17,7 +17,7 @@ var config = {
             {
                 test: /\.jsx?/,
                 include: APP_DIR,
-                loader: 'babel'
+                loader: 'babel-loader'
             }
         ]
     }
