3edce65942f4a5bf06c05588cbf7415b1e507d4a
Geoffrey J. Teale <tealeg@gmail.com>
2017-04-22 21:21:31 +0200

    Add UploadFile component.

--- jsx/app.jsx
+++ jsx/app.jsx
@@ -1,10 +1,66 @@
 import React from 'react';
 import {render} from 'react-dom';
 
+class UploadFile extends React.Component {
+
+    constructor(props) {
+	super(props);
+	this.state = {
+	    file: null,
+	}
+	this.handleChange = this.handleChange.bind(this);
+	this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange(event) {
+	this.setState({
+	    file: event.target.files[0],
+	});
+    }
+
+
+    sendData() {
+	var formData  = new FormData();
+	formData.append("file", this.state.file, this.state.file.name);
+
+	fetch("/api/conversion", {
+	    method: 'POST',
+	    body: formData
+	}).then(function (response) {
+	    console.log(response.json());
+	    
+	}).catch(function (error) {
+	    console.log(error);
+	});
+    }
+
+    handleSubmit(event) {
+	event.preventDefault();
+	this.sendData();
+    }
+
+    render() {
+	return (
+	    <form encType="multipart/form-data" onChange={this.handleChange} onSubmit={this.handleSubmit}>
+		<input type="file" name="uploadfile" />
+		<input type="submit" value="upload" />
+	    </form>
+	);
+    }
+}
+
 class App extends React.Component {
-  render () {
-    return <p> Hello React!</p>;
-  }
+    render () {
+	return (
+	    <div>
+		<p> Hello React!</p>
+		<UploadFile />
+	    </div>
+	);
+    }
 }
 
 render(<App/>, document.getElementById('app'));
+
+
+
