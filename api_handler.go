3276c3b90b92ac975197f29d85b9cc5e00d8a8de
Geoffrey J. Teale <tealeg@gmail.com>
2017-04-22 21:39:12 +0200

    Grab the uploaded file data.

--- api_handler.go
+++ api_handler.go
@@ -3,32 +3,41 @@ package main
 import (
 	"encoding/json"
 	"fmt"
-	"strings"
+	"io/ioutil"
 
 	"github.com/pilu/traffic"
 )
 
 type ExcelData struct {
 	DocumentName string
+	Data []byte
 }
 
-func excelResponse(w traffic.ResponseWriter) {
- 	excelData := ExcelData{DocumentName: "test"}
+func excelResponse(w traffic.ResponseWriter, r *traffic.Request) {
+	file, handler, err := r.FormFile("file") 
+        if err != nil { 
+                fmt.Println(err) 
+        } 
+        data, err := ioutil.ReadAll(file) 
+        if err != nil { 
+                fmt.Println(err) 
+        }
+	fmt.Print("Filename: ")
+	fmt.Println(handler.Filename)
+        // err = ioutil.WriteFile(handler.Filename, data, 0777) 
+        // if err != nil { 
+        //         fmt.Println(err) 
+        // } 
+	
+ 	excelData := ExcelData{DocumentName: "test", Data: data}
 	enc := json.NewEncoder(w)
 	enc.Encode(excelData)
 }
 
 func APIHandler(w traffic.ResponseWriter, r *traffic.Request) {
 	params := r.URL.Query()
-	r.ParseForm()
-	fmt.Print("Form: ")
-	fmt.Println(r.Form)
-	for k, v := range r.Form {
-		fmt.Println("key:", k)
-		fmt.Println("val:", strings.Join(v, ""))
-	}
 	call := params.Get("call")
 	if call == "conversion" {
-		excelResponse(w);
+		excelResponse(w, r);
 	}
 }
