42686692b05aa5c2383e207c37016fc0d28552d9
Geoffrey J. Teale <geoffrey.teale@canonical.com>
2017-04-27 16:47:13 +0200

    Pass back more structured JSON.

--- api_handler.go
+++ api_handler.go
@@ -9,10 +9,29 @@ import (
 	"github.com/tealeg/xlsx"
 )
 
+
+type Col struct {
+	ColID int
+	Value string
+}
+
+type Row struct {
+	RowID int
+	Cols []Col
+}
+
+type Sheet struct {
+	SheetID int
+	MaxCols int
+	MaxRows int
+	Rows []Row
+}
+
 type ExcelData struct {
 	DocumentName string
-	Sheets [][][]string
+	Sheets []Sheet
 }
+
 
 func excelResponse(w traffic.ResponseWriter, r *traffic.Request) {
 	file, handler, err := r.FormFile("file") 
@@ -30,11 +49,28 @@ func excelResponse(w traffic.ResponseWriter, r *traffic.Request) {
 	if err != nil {
 		fmt.Println(err)
 	}
-	
- 	excelData := ExcelData{DocumentName: handler.Filename}
-	excelData.Sheets, err = xlFile.ToSlice()
-	if err != nil {
-		fmt.Println(err)
+
+	excelData := ExcelData{DocumentName: handler.Filename}
+
+	excelData.Sheets = []Sheet{}
+	for sheetIndex, sheet := range xlFile.Sheets {
+		jSheet := Sheet{MaxCols: sheet.MaxCol, MaxRows: sheet.MaxRow, SheetID: sheetIndex}
+		jSheet.Rows = []Row{}
+		for rowIndex, row := range sheet.Rows {
+			jRow := Row{RowID: rowIndex}
+			jRow.Cols = []Col{}
+			for cellIndex, cell := range row.Cells {
+				jCol := Col{ColID: cellIndex}
+				value, err := cell.FormattedValue()
+				if err != nil {
+					value = err.Error()
+				}
+				jCol.Value = value
+				jRow.Cols = append(jRow.Cols, jCol)
+			}
+			jSheet.Rows = append(jSheet.Rows, jRow)
+		}
+		excelData.Sheets = append(excelData.Sheets, jSheet)
 	}
 	fmt.Println(excelData)
 	enc := json.NewEncoder(w)
--- api_handler_test.go
+++ api_handler_test.go
@@ -10,6 +10,4 @@ func TestAPIHandler(t *testing.T) {
 	if recorder.Code != expectedStatusCode {
 		t.Errorf("Expected response status code `%d`, got `%d`", expectedStatusCode, recorder.Code)
 	}
-
-	
 }
